6b9ab7a62a4e8bfa0f64785d6e9dc0dbb7d8353e
palwashakmalik <palwashamalikt@gmail.com>
2023-12-12 01:28:41 +0500

    added test cases for GET request for articles

--- news_site/news_app/tests.py
+++ news_site/news_app/tests.py
@@ -1,1 +1,34 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from .views import news_articles_view
+from unittest.mock import patch
+import os
+
+
+class NewsAPITest(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_news_articles_view(self):
+        # Test case 1: All parameters provided
+        request = self.factory.get(
+            '/news', {'q': 'netflix', 'from': "2023-12-10", 'sortBy': 'publishedAt', 'language': 'en'})
+        response = news_articles_view(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Test case 2: Missing some parameters
+        request = self.factory.get('/news', {'q': 'google', 'language': 'en'})
+        response = news_articles_view(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Test case 3: Missing all parameters
+        request = self.factory.get('/news')
+        response = news_articles_view(request)
+        self.assertEqual(response.status_code, 500)
+
+    @patch.dict(os.environ, {'NEWS_API_KEY': '12345'})
+    # Test case 4: Wrong API key
+    def test_wrong_api_key(self):
+        request = self.factory.get('/news', {'q': 'netflix', 'language': 'en'})
+        response = news_articles_view(request)
+        self.assertEqual(response.status_code, 500)
