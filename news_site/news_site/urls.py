e77e3c449b0532327c81ee4eed6768b1aa12a737
palwashakmalik <palwashamalikt@gmail.com>
2023-12-12 19:08:34 +0500

    removed news_site folder

--- news_site/news_site/urls.py
+++
@@ -1,9 +0,0 @@
-"""news_site URL Configuration
-"""
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('', include('news_app.urls')),
-    path('admin/', admin.site.urls),
-]
